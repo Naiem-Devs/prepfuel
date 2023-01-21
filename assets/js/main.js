9ea1a103afdd3c3184699b8bdc597f3786b5c436
Naiem-Devs <dnsnaiem@gmail.com>
2023-01-22 00:57:19 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+(function($) {
+  "use strict";
+  
+ // menu 
+  $('.siteBar-btn').click( function (){ 
+    $('.mobile-menu').toggleClass('siteBar');   
+  }); 
+  
+ // menu 
+  $('.rv_btn').click( function (){ 
+    $('.rev-menu').toggleClass('rv_sitevar');   
+  }); 
+
+
+ 
+})(jQuery);
